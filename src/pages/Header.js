d01f6b5c70b622d4e3b978e1cfcd940f630122c8
minsu kim <kimminsu31415@gmail.com>
2024-08-10 16:26:48 +0900

    fix: develop 병합

--- src/pages/Header.js
+++ src/pages/Header.js
@@ -7,7 +7,7 @@ const Header = () => {
       <h1 className="bg-emerald-200">Header입니다.</h1>
       <div className="grid grid-cols-3 gap-4 p-4">
         <Link to="/">Main으로 이동</Link>
-        <Link to="/PageA">Page A로 이동</Link>
+        <Link to="/SelectPage1">선택화면 1로 이동</Link>
         <Link to="/pageB">Page B로 이동</Link>
         <Link to="/pageC">Page C로 이동</Link>
       </div>
