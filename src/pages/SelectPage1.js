4d29319fa35a413b08df38e72410d41299272249
Green-JEONG <th2gr22n@gmail.com>
2024-08-10 19:14:02 +0900

    [Completed] SelectPage1 - 퍼블리싱

--- src/pages/SelectPage1.js
+++ src/pages/SelectPage1.js
@@ -2,22 +2,56 @@ import React from 'react';
 
 const SelectPage1 = () => {
   return (
-    <div>
+    <di className="flex justify-center">
+    <div className="flex flex-col justify-center items-center max-w-[400px] w-full bg-black">
       {/* Header 시작 */}
-      <div className="h-72 w-72 mt-24 pm-10" style={{ backgroundImage: `url('/assets/Logo.png')`, backgroundSize: 'cover', backgroundPosition: 'center', }}
-            ></div>
-        <header className="" alt="Logo">
-        </header>
+      <header className="">
+        <img
+            src="/assets/SelectPage1/Logo.png"
+            alt="Logo"
+            className="w-32 my-7"
+        />
+      </header>
       {/* Header 끝 */}
       {/* Character 설명 시작 */}
+      <div className="relative">
+        <img
+          src="/assets/SelectPage1/Character1.png"
+          alt="Character1"
+          className="w-89"
+        />
+        <div className='flex flex-row absolute text-white bottom-[7.5rem] left-5 gap-x-[0.3rem]'>
+          <p className="text-[2rem]">
+            성진우
+          </p>
+          <p className="pt-5 text-[1rem]">
+            25
+          </p>
+        </div>
+        <div className="absolute text-white  bottom-[6rem] left-[1.6rem] text-[1.4rem]">
+        &quot;...일어나라&quot;
+        </div>
+        <div className="flex flex-row justify-between ">
+        <div className="absolute text-white bottom-[4rem] left-[1.5rem] text-[1rem] rounded-md py-[0.01rem] px-[0.5rem] bg-[#FCC729]">#상남자</div>
+        <div className="absolute text-white bottom-[4rem] left-[6.2rem] text-[1rem] rounded-md py-[0.01rem] px-[0.5rem] bg-[#FCC729]">#초능력자</div>
+        <div className="absolute text-white bottom-[4rem] left-[11.7rem] text-[1rem] rounded-md py-[0.01rem] px-[0.5rem] bg-[#FCC729]">#쿨내</div>
+        </div>
       {/* Character 설명 끝 */}
       {/* 별점 시작 */}
-
+        <img
+            src="/assets/SelectPage1/Heart.png"
+            alt="Heart"
+            className="absolute w-80 bottom-[0.4rem] left-[1.7rem]"
+            />
       {/* 별점 끝 */}
+      </div>
       {/* Button 시작 */}
-        <div className=""></div>
+        <div className="my-[3rem] rounded-md py-[0.5rem] px-[6rem] bg-white">
+          다른 주인공 만나러 가기 💖
+        </div>
       {/* Button 끝 */}
     </div>
+    </di>
   );
 };
 
